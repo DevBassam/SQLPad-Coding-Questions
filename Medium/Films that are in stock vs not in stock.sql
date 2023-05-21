6e4fc0a37fe6ee2adf5d2725b6f2b28e8da2e351
Bassam Salama <84365449+DevBassam@users.noreply.github.com>
2023-05-21 17:52:50 +0300

    Create Films that are in stock vs not in stock.sql

---
+++ Medium/Films that are in stock vs not in stock.sql
@@ -0,0 +1,19 @@
+--Problem Link: https://sqlpad.io/questions/42/films-that-are-in-stock-vs-not-in-stock/
+
+WITH total_film AS (
+	SELECT COUNT(*) AS count
+	FROM film
+	)
+	,
+inventory_film AS (
+	SELECT COUNT(DISTINCT film_id) AS count
+	FROM inventory
+	)
+SELECT CASE WHEN inventory_film.count > 0	THEN 'in stock'	END AS in_stock
+	,CASE WHEN inventory_film.count > 0 THEN inventory_film.count END AS count
+FROM inventory_film
+
+UNION ALL
+
+SELECT 'not in stock', total_film.count - inventory_film.count
+FROM total_film, inventory_film
