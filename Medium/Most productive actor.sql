2ea9b43ec1661b64407f87ff649453b073361939
Bassam Salama <84365449+DevBassam@users.noreply.github.com>
2023-05-22 17:22:27 +0300

    Update Most productive actor.sql

--- Medium/Most productive actor.sql
+++ Medium/Most productive actor.sql
@@ -1,9 +1,9 @@
 -- Problem Link: https://sqlpad.io/questions/19/most-productive-actor/
 
 SELECT actor.first_name, actor.last_name
-FROM actor
-JOIN film_actor 
-ON actor.actor_id = film_actor.actor_id
-GROUP BY actor.actor_id, actor.first_name, actor.last_name
+FROM actor As ac
+JOIN film_actor As fa 
+ON ac.actor_id = fa.actor_id
+GROUP BY ac.actor_id, ac.first_name, ac.last_name
 ORDER BY COUNT(*) DESC
-LIMIT 1;
+LIMIT 1
